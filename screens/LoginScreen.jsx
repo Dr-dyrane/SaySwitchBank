8caee3b16f33fcc12ed5ce0b889740a320dd358f
Dr Dyrane Alexander <Ogranya.Alex@gmail.com>
2024-10-10 16:33:41 +0100

    add login

--- screens/LoginScreen.jsx
+++ screens/LoginScreen.jsx
@@ -1,5 +1,14 @@
-import React, { useState } from "react";
-import { View, Text, Pressable, ActivityIndicator, Alert, ScrollView } from "react-native";
+import React, { useEffect, useState } from "react";
+import {
+	View,
+	Text,
+	Pressable,
+	ActivityIndicator,
+	Alert,
+	ScrollView,
+	Image,
+	Keyboard,
+} from "react-native";
 import { Formik } from "formik";
 import * as Yup from "yup";
 import Input from "../components/form/Input"; // Reusable Input Component
@@ -22,6 +31,7 @@ const LoginScreen = () => {
 	const { login: loginUser } = useLogin();
 	const router = useRouter();
 	const { showToast } = useToast();
+	const [keyboardVisible, setKeyboardVisible] = useState(false); // Track keyboard visibility
 
 	// Simulate Login API Call
 	const handleLogin = async (values) => {
@@ -41,76 +51,109 @@ const LoginScreen = () => {
 		}
 	};
 
+	useEffect(() => {
+		// Add event listeners for keyboard open and close
+		const keyboardDidShowListener = Keyboard.addListener(
+			"keyboardDidShow",
+			() => {
+				setKeyboardVisible(true);
+			}
+		);
+		const keyboardDidHideListener = Keyboard.addListener(
+			"keyboardDidHide",
+			() => {
+				setKeyboardVisible(false);
+			}
+		);
+
+		return () => {
+			// Remove event listeners on cleanup
+			keyboardDidShowListener.remove();
+			keyboardDidHideListener.remove();
+		};
+	}, []);
+
 	return (
 		<LinearGradient
 			colors={["#fff", "#f0fff4", "#fff"]}
-			className="flex-1 justify-center items-center p-6 pb-16 pt-14 bg-backgroundLight"
+			className="flex-1 justify-between items-center p-6 bg-backgroundLight"
 		>
-			<View className="mb-6 justify-center space-y-2">
-				<Text className="text-6xl text-center font-[900] text-primary">
-					Welcome Back
-				</Text>
-				<Text className="text-lg text-center text-gray-500">
-					Login to your account
-				</Text>
+			<View className="flex flex-col flex-shrink">
+				<View className="flex-1 justify-center space-y-2">
+					<Text className="text-6xl text-center font-[900] text-primary">
+						Welcome Back
+					</Text>
+					<Text className="text-lg text-center text-gray-500">
+						Login to your account
+					</Text>
+				</View>
+				{!keyboardVisible && (
+					<Image
+						source={require("../assets/sign/login.png")}
+						className="contain w-[320px] h-[320px]"
+						resizeMode="contain"
+					/>
+				)}
 			</View>
-			<ScrollView className="w-full">
-				<Formik
-					initialValues={{ email: "", password: "" }}
-					validationSchema={LoginSchema}
-					onSubmit={handleLogin}
-				>
-					{({
-						handleChange,
-						handleBlur,
-						handleSubmit,
-						values,
-						errors,
-						touched,
-					}) => (
-						<>
-							{/* Email Input */}
-							<Input
-								label="Email"
-								placeholder="Enter your email"
-								icon="mail" // Pass the email icon
-								onChangeText={handleChange("email")}
-								onBlur={handleBlur("email")}
-								value={values.email}
-								error={touched.email && errors.email}
-							/>
 
-							{/* Password Input */}
-							<Input
-								label="Password"
-								placeholder="Enter your password"
-								icon="lock-closed" // Pass the password icon
-								secureTextEntry
-								onChangeText={handleChange("password")}
-								onBlur={handleBlur("password")}
-								value={values.password}
-								error={touched.password && errors.password}
-							/>
+			<View className="w-full">
+				<ScrollView>
+					<Formik
+						initialValues={{ email: "", password: "" }}
+						validationSchema={LoginSchema}
+						onSubmit={handleLogin}
+					>
+						{({
+							handleChange,
+							handleBlur,
+							handleSubmit,
+							values,
+							errors,
+							touched,
+						}) => (
+							<>
+								{/* Email Input */}
+								<Input
+									label="Email"
+									placeholder="Enter your email"
+									icon="mail" // Pass the email icon
+									onChangeText={handleChange("email")}
+									onBlur={handleBlur("email")}
+									value={values.email}
+									error={touched.email && errors.email}
+								/>
 
-							{/* Submit Button */}
-							<Pressable
-								onPress={handleSubmit}
-								disabled={loading}
-								className="w-full bg-primary rounded-xl py-4 text-lg mt-4 flex flex-row px-6 items-center justify-between space-x-4"
-								android_ripple={{ color: "#333" }}
-							>
-								<Text className="text-white text-xl">
-									{loading ? "Logging in..." : "Login"}
-								</Text>
-								<View className="w-8 h-8 bg-none border border-white rounded-full justify-center items-center">
-									<Ionicons name="arrow-forward" size={18} color="white" />
-								</View>
-							</Pressable>
-						</>
-					)}
-				</Formik>
-			</ScrollView>
+								{/* Password Input */}
+								<Input
+									label="Password"
+									placeholder="Enter your password"
+									icon="lock-closed" // Pass the password icon
+									secureTextEntry
+									onChangeText={handleChange("password")}
+									onBlur={handleBlur("password")}
+									value={values.password}
+									error={touched.password && errors.password}
+								/>
 
+								{/* Submit Button */}
+								<Pressable
+									onPress={handleSubmit}
+									disabled={loading}
+									className="w-full bg-primary rounded-xl py-4 text-lg mt-4 flex flex-row px-6 items-center justify-between space-x-4"
+									android_ripple={{ color: "#333" }}
+								>
+									<Text className="text-white text-xl">
+										{loading ? "Logging in..." : "Login"}
+									</Text>
+									<View className="w-8 h-8 bg-none border border-white rounded-full justify-center items-center">
+										<Ionicons name="arrow-forward" size={18} color="white" />
+									</View>
+								</Pressable>
+							</>
+						)}
+					</Formik>
+				</ScrollView>
+			</View>
 			{/* Loading Indicator */}
 			{loading && (
 				<ActivityIndicator size="large" color="#4CAF50" className="mt-4" />
--- screens/SignupScreen.jsx
+++ screens/SignupScreen.jsx
@@ -1,5 +1,13 @@
-import React, { useState } from "react";
-import { View, Text, Pressable, ActivityIndicator, Alert, ScrollView } from "react-native";
+import React, { useState, useEffect } from "react";
+import {
+	View,
+	Text,
+	Pressable,
+	ActivityIndicator,
+	ScrollView,
+	Image,
+	Keyboard,
+} from "react-native";
 import { Formik } from "formik";
 import * as Yup from "yup";
 import Input from "../components/form/Input"; // Reusable Input Component
@@ -23,25 +31,45 @@ const SignupSchema = Yup.object().shape({
 
 const SignupScreen = () => {
 	const [loading, setLoading] = useState(false);
+	const [keyboardVisible, setKeyboardVisible] = useState(false); // Track keyboard visibility
 	const { showToast } = useToast();
 	const router = useRouter();
 	const { signUp } = useSignUp();
+
+	useEffect(() => {
+		// Add event listeners for keyboard open and close
+		const keyboardDidShowListener = Keyboard.addListener(
+			"keyboardDidShow",
+			() => {
+				setKeyboardVisible(true);
+			}
+		);
+		const keyboardDidHideListener = Keyboard.addListener(
+			"keyboardDidHide",
+			() => {
+				setKeyboardVisible(false);
+			}
+		);
+
+		return () => {
+			// Remove event listeners on cleanup
+			keyboardDidShowListener.remove();
+			keyboardDidHideListener.remove();
+		};
+	}, []);
 
 	// Simulate Signup API Call
 	const handleSignup = async (values) => {
 		setLoading(true);
 		try {
-			const { email, password, username } = values; // Extract email, password, and username from values
+			const { email, password, username } = values;
 			const isSignedUp = await signUp({ email, password, username });
 			if (isSignedUp) {
-				// Navigate to a different screen or show a success message
 				router.replace("/(tabs)"); // Navigate on successful login
-				showToast("Sign-up successful!", "success"); // Show success toast
+				showToast("Sign-up successful!", "success");
 			}
 		} catch (error) {
-			//console.error("Sign-up error:", error); // Log error for debugging
-			// Handle error (e.g., show a toast or alert)
-			showToast("Sign-up failed: " + error.message, "error"); // Show error toast
+			showToast("Sign-up failed: " + error.message, "error");
 		} finally {
 			setLoading(false);
 		}
@@ -50,102 +78,108 @@ const SignupScreen = () => {
 	return (
 		<LinearGradient
 			colors={["#fff", "#f0fff4", "#fff"]}
-			className="flex-1 justify-center items-center p-6 pb-16 pt-14 bg-backgroundLight"
+			className="flex-1 justify-between items-center p-6 bg-backgroundLight"
 		>
-			<View className="mb-6 justify-center space-y-2">
-				<Text className="text-6xl text-center font-[900] text-primary">
-					Register
-				</Text>
-				<Text className="text-lg text-center text-gray-500">
-					Create your new account
-				</Text>
+			{!keyboardVisible && (
+				<View className="flex flex-col flex-shrink">
+					{/* Conditionally render the image if the keyboard is not visible */}
+					<View className="justify-center space-y-2 flex-1">
+						<Text className="text-6xl text-center font-[900] text-primary">
+							Register
+						</Text>
+						<Text className="text-lg text-center text-gray-500">
+							Create your new account
+						</Text>
+					</View>
+
+					<Image
+						source={require("../assets/sign/signup.png")}
+						className="w-[220px] h-[220px]"
+						resizeMode="contain"
+					/>
+				</View>
+			)}
+			<View className="w-full">
+				<ScrollView>
+					<Formik
+						initialValues={{
+							username: "",
+							email: "",
+							password: "",
+							confirmPassword: "",
+						}}
+						validationSchema={SignupSchema}
+						onSubmit={handleSignup}
+					>
+						{({
+							handleChange,
+							handleBlur,
+							handleSubmit,
+							values,
+							errors,
+							touched,
+						}) => (
+							<>
+								<Input
+									label="Username"
+									placeholder="Enter your username"
+									icon="person"
+									onChangeText={handleChange("username")}
+									onBlur={handleBlur("username")}
+									value={values.username}
+									error={touched.username && errors.username}
+								/>
+
+								<Input
+									label="Email"
+									placeholder="Enter your email"
+									icon="mail"
+									onChangeText={handleChange("email")}
+									onBlur={handleBlur("email")}
+									value={values.email}
+									error={touched.email && errors.email}
+								/>
+
+								<Input
+									label="Password"
+									placeholder="Enter your password"
+									secureTextEntry
+									icon="lock-closed"
+									onChangeText={handleChange("password")}
+									onBlur={handleBlur("password")}
+									value={values.password}
+									error={touched.password && errors.password}
+								/>
+
+								<Input
+									label="Confirm Password"
+									placeholder="Confirm your password"
+									secureTextEntry
+									icon="lock-closed"
+									onChangeText={handleChange("confirmPassword")}
+									onBlur={handleBlur("confirmPassword")}
+									value={values.confirmPassword}
+									error={touched.confirmPassword && errors.confirmPassword}
+								/>
+
+								<Pressable
+									onPress={handleSubmit}
+									disabled={loading}
+									className="w-full bg-primary rounded-xl py-4 text-lg mt-4 flex flex-row px-6 items-center justify-between space-x-4"
+									android_ripple={{ color: "#333" }}
+								>
+									<Text className="text-white text-lg">
+										{loading ? "Signing up..." : "Sign Up"}
+									</Text>
+									<View className="w-8 h-8 bg-none border border-white rounded-full justify-center items-center">
+										<Ionicons name="arrow-forward" size={18} color="white" />
+									</View>
+								</Pressable>
+							</>
+						)}
+					</Formik>
+				</ScrollView>
 			</View>
-			<ScrollView className="w-full">
-				<Formik
-					initialValues={{
-						username: "",
-						email: "",
-						password: "",
-						confirmPassword: "",
-					}}
-					validationSchema={SignupSchema}
-					onSubmit={handleSignup}
-				>
-					{({
-						handleChange,
-						handleBlur,
-						handleSubmit,
-						values,
-						errors,
-						touched,
-					}) => (
-						<>
-							{/* Username Input */}
-							<Input
-								label="Username"
-								placeholder="Enter your username"
-								icon="person" // Icon for username
-								onChangeText={handleChange("username")}
-								onBlur={handleBlur("username")}
-								value={values.username}
-								error={touched.username && errors.username}
-							/>
-
-							{/* Email Input */}
-							<Input
-								label="Email"
-								placeholder="Enter your email"
-								icon="mail" // Icon for email
-								onChangeText={handleChange("email")}
-								onBlur={handleBlur("email")}
-								value={values.email}
-								error={touched.email && errors.email}
-							/>
-
-							{/* Password Input */}
-							<Input
-								label="Password"
-								placeholder="Enter your password"
-								secureTextEntry
-								icon="lock-closed" // Icon for password
-								onChangeText={handleChange("password")}
-								onBlur={handleBlur("password")}
-								value={values.password}
-								error={touched.password && errors.password}
-							/>
-
-							{/* Confirm Password Input */}
-							<Input
-								label="Confirm Password"
-								placeholder="Confirm your password"
-								secureTextEntry
-								icon="lock-closed" // Icon for confirm password
-								onChangeText={handleChange("confirmPassword")}
-								onBlur={handleBlur("confirmPassword")}
-								value={values.confirmPassword}
-								error={touched.confirmPassword && errors.confirmPassword}
-							/>
-
-							{/* Submit Button */}
-							<Pressable
-								onPress={handleSubmit}
-								disabled={loading}
-								className="w-full bg-primary rounded-xl py-4 text-lg mt-4 flex flex-row px-6 items-center justify-between space-x-4"
-								android_ripple={{ color: "#333" }}
-							>
-								<Text className="text-white text-lg">
-									{loading ? "Signing up..." : "Sign Up"}
-								</Text>
-								<View className="w-8 h-8 bg-none border border-white rounded-full justify-center items-center">
-									<Ionicons name="arrow-forward" size={18} color="white" />
-								</View>
-							</Pressable>
-						</>
-					)}
-				</Formik>
-			</ScrollView>
-
-			{/* Loading Indicator */}
 			{loading && (
 				<ActivityIndicator size="large" color="#4CAF50" className="mt-4" />
 			)}
