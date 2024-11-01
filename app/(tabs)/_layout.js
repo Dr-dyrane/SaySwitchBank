9b122ec42bb46021fd7da27841d29ef611f0de25
Dr Dyrane Alexander <Ogranya.Alex@gmail.com>
2024-11-01 23:39:24 +0100

    modified:   app/(tabs)/_layout.js

--- app/(tabs)/_layout.js
+++ app/(tabs)/_layout.js
@@ -1,8 +1,8 @@
 import { Tabs } from "expo-router";
 import { AntDesign, Ionicons } from "@expo/vector-icons"; // Import Ionicons for tab icons
 import { useRouter } from "expo-router";
-import React from "react";
-import { Image, Text, TouchableOpacity, View } from "react-native";
+import React, { useEffect, useRef } from "react";
+import { Animated, Image, Text, TouchableOpacity, View } from "react-native";
 import { useAuth } from "../../contexts/AuthContext";
 import { useToast } from "../../contexts/ToastContext";
 
@@ -10,6 +10,26 @@ export default function TabsLayout() {
 	const router = useRouter();
 	const { logout, user } = useAuth();
 	const { showToast } = useToast();
+
+	const pingAnim = useRef(new Animated.Value(1)).current;
+
+	// Ping animation setup
+	useEffect(() => {
+		Animated.loop(
+			Animated.sequence([
+				Animated.timing(pingAnim, {
+					toValue: 2,
+					duration: 800,
+					useNativeDriver: true,
+				}),
+				Animated.timing(pingAnim, {
+					toValue: 1,
+					duration: 800,
+					useNativeDriver: true,
+				}),
+			])
+		).start();
+	}, [pingAnim]);
 
 	return (
 		<Tabs
@@ -25,6 +45,7 @@ export default function TabsLayout() {
 					fontWeight: "bold", // Bold font for the title
 					fontSize: 18, // Adjust the size if needed
 				},
+				//tabBarLabel: () => null, // Hide label for More tab
 				headerShadowVisible: false,
 				headerLeft: () => (
 					<Ionicons
@@ -87,28 +108,37 @@ export default function TabsLayout() {
 					),
 					headerRight: () => (
 						<TouchableOpacity
-							style={{ marginRight: 16 }}
-							onPress={async () => {
-								const result = await logout(); // Call the logout function
-								if (result.success) {
-									// Show success toast if logout is successful
-									showToast(result.message, "success");
-									router.replace("/(auth)"); // Redirect to the auth stack (login/signup screen)
-								} else {
-									// Show error toast if logout fails
-									showToast(result.message, "error");
-								}
-							}}
+							style={{ marginRight: 18 }}
+							onPress={() => router.push("/notifications")}
 						>
-							{/* Logo with text */}
-							<View style={{ flexDirection: "row", alignItems: "center" }}>
-								<AntDesign name="logout" size={14} color="red" />
-								<Text
-									style={{ fontWeight: "bold", fontSize: 16 }}
-									className="text-red-500 ml-2"
-								>
-									Logout
-								</Text>
+							<View style={{ position: "relative" }}>
+								<Ionicons name="notifications" size={18} color="slategray" />
+
+								{/* Ping Badge */}
+								<View style={{ position: "absolute", top: -4, right: -4 }}>
+									<Animated.View
+										style={{
+											position: "absolute",
+											width: 10,
+											height: 10,
+											borderRadius: 999,
+											backgroundColor: "rgba(56, 178, 172, 0.5)", // Teal color with transparency
+											transform: [{ scale: pingAnim }],
+											opacity: pingAnim.interpolate({
+												inputRange: [1, 2],
+												outputRange: [1, 0],
+											}),
+										}}
+									/>
+									<View
+										style={{
+											width: 10,
+											height: 10,
+											borderRadius: 999,
+											backgroundColor: "teal",
+										}}
+									/>
+								</View>
 							</View>
 						</TouchableOpacity>
 					),
@@ -155,13 +185,15 @@ export default function TabsLayout() {
 					tabBarIcon: ({ color, size, focused }) => (
 						<Ionicons
 							name={
-								focused ? "ellipsis-horizontal-circle-sharp" : "ellipsis-horizontal-circle"
+								focused
+									? "ellipsis-horizontal-circle-sharp"
+									: "ellipsis-horizontal-circle"
 							}
 							color={color}
 							size={24}
 						/>
 					),
-					headerTitle: 'More Options',
+					headerTitle: "More Options",
 					headerShown: true,
 				}}
 			/>
