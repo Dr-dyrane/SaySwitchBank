473a4cd76d54c11c1f8fa5851f229bb4abc7a1a3
Dr Dyrane Alexander <Ogranya.Alex@gmail.com>
2024-11-02 23:45:55 +0100

    modified:   components/payment/PayoutModals.jsx

--- components/payment/PayoutModals.jsx
+++ components/payment/PayoutModals.jsx
@@ -81,39 +81,101 @@ export function ConfirmationModal({
 			<TouchableWithoutFeedback onPress={onClose}>
 				<View className="flex-1 justify-end bg-black/50">
 					<TouchableWithoutFeedback>
-						<View className="bg-white rounded-t-3xl p-6 h-1/2">
-							<Text className="text-xl font-semibold mb-4">Confirm Payout</Text>
-							<View className="mb-4">
-								<Text className="text-gray-500">Amount</Text>
-								<Text className="text-lg font-semibold">
-									₦{parseInt(amount).toLocaleString()}
-								</Text>
-							</View>
-							<View className="mb-4">
-								<Text className="text-gray-500">Account</Text>
-								<Text className="text-lg font-semibold">
-									{selectedAccount?.name}
-								</Text>
-							</View>
-							{remark && (
-								<View className="mb-4">
-									<Text className="text-gray-500">Remark</Text>
-									<Text className="text-lg font-semibold">{remark}</Text>
+						<View>
+							{/* Drag Indicator Bar */}
+							<View
+								style={{
+									height: 4,
+									width: 80,
+									backgroundColor: "#ccc", // Customize color
+									borderRadius: 2,
+									alignSelf: "center",
+									marginBottom: 8,
+								}}
+							/>
+							<View className="bg-white rounded-t-3xl py-6 px-4">
+								<View className="justify-center items-center mb-6">
+									<Text className="text-2xl font-semibold">
+										₦{parseInt(amount).toLocaleString()}
+									</Text>
 								</View>
-							)}
-							<View className="flex-row justify-between mt-auto">
-								<TouchableOpacity
-									onPress={onClose}
-									className="bg-gray-300 p-3 rounded-lg flex-1 mr-2"
-								>
-									<Text className="text-gray-800 text-center">Cancel</Text>
-								</TouchableOpacity>
-								<TouchableOpacity
-									onPress={onConfirm}
-									className="bg-teal-600 p-3 rounded-lg flex-1 ml-2"
-								>
-									<Text className="text-white text-center">Confirm</Text>
-								</TouchableOpacity>
+								{selectedAccount && (
+									<>
+										<View className="flex-row items-center justify-between py-2 mb-3 border-gray-200 bg-slate-50 rounded-xl px-4">
+											<View className="flex-row items-center">
+												<View className="bg-[#E5F5F1] p-2 rounded-full mr-3">
+													<Ionicons
+														name={selectedAccount.icon}
+														size={24}
+														color="#008773"
+													/>
+												</View>
+
+												<View>
+													<Text className="text-md text-gray-800">
+														{selectedAccount.name}
+													</Text>
+													<Text className="text-gray-500">
+														{selectedAccount.number}
+													</Text>
+												</View>
+											</View>
+											<Ionicons
+												name="checkmark-done-circle"
+												size={18}
+												color="teal"
+											/>
+										</View>
+										<View className="flex-row justify-between w-full mb-2 p-2">
+											<View className="items-center justify-center">
+												<Text className="text-gray-500">Bank Name</Text>
+											</View>
+											<View className="items-center justify-center">
+												<Text className="font-semibold">
+													{selectedAccount.name}
+												</Text>
+											</View>
+										</View>
+										<View className="flex-row justify-between w-full mb-2 p-2">
+											<View className="items-center justify-center">
+												<Text className="text-gray-500">Account Number</Text>
+											</View>
+											<View className="items-center justify-center">
+												<Text className="font-semibold">
+													{selectedAccount.number}
+												</Text>
+											</View>
+										</View>
+									</>
+								)}
+								<View className="flex-row justify-between w-full mb-2 p-2">
+									<View className="items-center justify-center">
+										<Text className="text-gray-500">Amount</Text>
+									</View>
+									<View className="items-center justify-center">
+										<Text className="font-semibold">
+											₦{parseInt(amount).toLocaleString()}
+										</Text>
+									</View>
+								</View>
+								{remark && (
+									<View className="flex-row justify-between w-full mb-2 p-2">
+										<View className="items-center justify-center">
+											<Text className="text-gray-500">Remark</Text>
+										</View>
+										<View className="items-center justify-center">
+											<Text className="font-semibold">{remark}</Text>
+										</View>
+									</View>
+								)}
+								<View className="flex-row justify-center mt-8">
+									<TouchableOpacity
+										onPress={onConfirm}
+										className="bg-primary p-3 rounded-2xl w-full"
+									>
+										<Text className="text-white text-center text-lg">Pay</Text>
+									</TouchableOpacity>
+								</View>
 							</View>
 						</View>
 					</TouchableWithoutFeedback>
